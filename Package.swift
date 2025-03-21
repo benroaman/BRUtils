046c21d12904e16187cfd630cc8025f354de74ec
Ben Roaman <benroaman@Bens-MacBook-Pro-3.local>
2025-03-21 11:02:13 -0400

    added min requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "BRUtils",
+    platforms: [.macOS(.v12),
+                .iOS(.v15),
+                .watchOS(.v8),
+                .macCatalyst(.v15),
+                .tvOS(.v15),
+                .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
